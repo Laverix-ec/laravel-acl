44f1f943332d236997d2f6e480bc68df47e230a8
Santiago Arellano <santiago.arellano@crifa.com>
2018-12-26 09:24:10 -0500

    remove user fields

--- src/migrations/acl/2017_04_25_092606_create_users_table.php
+++ src/migrations/acl/2017_04_25_092606_create_users_table.php
@@ -17,12 +17,6 @@ class CreateUsersTable extends Migration
             Schema::create('users', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('username',20);
-                $table->string('first_name', 50);
-                $table->string('last_name', 50);
-                $table->enum('document_type', ['CED', 'RUC', 'PAS']);
-                $table->string('document', 50);
-                $table->string('photo', 255)->nullable();
-                $table->string('email', 75);
                 $table->string('password', 255);
                 $table->rememberToken();
                 $table->boolean('changed_password')->default(false);
