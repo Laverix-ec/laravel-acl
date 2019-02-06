33329e39553a1188ad9c31861e2098671e8a8b0c
Esteban Grijalva <esteban.grijalva@crifa.com>
2019-02-06 16:31:36 -0500

    Added constants for each module.

--- src/SlonCorp/Acl/Models/Eloquent/Module.php
+++ src/SlonCorp/Acl/Models/Eloquent/Module.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Module extends Model
 {
     /**
+     * Modules constants
+     */
+    const MODULE_GXR = 1;
+    const MODULE_ROADS = 2;
+    const MODULE_CONFIGURATION = 3;
+
+    /**
      * The attributes that are fillable via mass assignment.
      *
      * @var array
